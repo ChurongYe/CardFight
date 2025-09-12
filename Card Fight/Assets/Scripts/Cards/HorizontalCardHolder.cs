14361afeba32fb0ed53afb304747ea4afc3aa87e
ChurongYe <yechurong0916@163.com>
2025-09-12 15:49:55 +0800

    3

--- Card Fight/Assets/Scripts/Cards/HorizontalCardHolder.cs
+++ Card Fight/Assets/Scripts/Cards/HorizontalCardHolder.cs
@@ -109,6 +109,7 @@ public class HorizontalCardHolder : MonoBehaviour
             }
 
         }
+        StartCoroutine(AutoRefreshCards());
     }
     void Shuffle<T>(List<T> list)
     {
@@ -389,6 +390,15 @@ public class HorizontalCardHolder : MonoBehaviour
             cardPool.Add(card);
             Debug.Log($"已将 {card} 放回卡池");
             Shuffle(cardPool);
+        }
+    }
+
+    private IEnumerator AutoRefreshCards()
+    {
+        while (true)
+        {
+            RefreshEmptyCards(); //调用你的方法
+            yield return new WaitForSeconds(15f); //等待5秒
         }
     }
     public void RefreshEmptyCards()//刷新卡牌
